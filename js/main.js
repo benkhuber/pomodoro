9cdc83cd5dc827cc1f6af698223c31d43bb9a6d0
Ben Huber <benkhuber@gmail.com>
2022-10-11 21:24:50 -0700

    countdown and rest countdown functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let clock = document.querySelector('#clock')
+let button = document.querySelector('#startTimer')
+let timer = document.querySelector('#countdown')
+button.addEventListener('click', getDeadline)
+button.addEventListener('click', countdown)
 
-function worldTime () {
+function worldTime() {
     let hours = new Date().getHours()
     let minutes = new Date().getMinutes()
     let seconds = new Date().getSeconds()
@@ -13,14 +17,62 @@ function worldTime () {
         hours = hours - 12
     }
 
-    if (hours < 10) {
-        hours = '0' + hours
+    if (minutes < 10) {
+        minutes = '0' + minutes
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds
     }
 
-    
-clock.innerHTML = `${hours}:${minutes}:${seconds} ${session}`
+    clock.innerHTML = `${hours}:${minutes} ${session}`
+    setTimeout(worldTime, 1000)
 }
 
-setInterval(1000)
+deadlineStore = 0
+
+function getDeadline() {
+    deadlineStore = Date.now() + 3000
+}
+
+function restDeadline() {
+    deadlineStore = Date.now() + 300000
+}
+
+
+
+function countdown() {
+    let deadline = deadlineStore
+    let currentTime = Date.now()
+    let timeLeft = deadline - currentTime
+    let seconds = Math.round(timeLeft / 1000)
+    let minutes = Math.floor(seconds / 60)
+    let displaySeconds = seconds - (minutes*60)
+    if (displaySeconds < 10) {
+        displaySeconds = '0' + displaySeconds
+    }
+
+    timer.innerHTML = `${minutes}:${displaySeconds}`
+    if (minutes == 0 && displaySeconds == 0) {
+        restDeadline()
+        restCountdown()
+    } else {
+    setTimeout(countdown, 1000)
+    }
+}
+
+function restCountdown() {
+    let restDeadline = deadlineStore
+    let currentTime = Date.now()
+    let timeLeft = restDeadline - currentTime
+    let seconds = Math.round(timeLeft / 1000)
+    let minutes = Math.floor(seconds / 60)
+    let displaySeconds = seconds - (minutes*60)
+    if (displaySeconds < 10) {
+        displaySeconds = '0' + displaySeconds
+    }
+    timer.innerHTML = `${minutes}:${displaySeconds}`
+    setTimeout(restCountdown, 1000)
+}
+
 
 worldTime()
