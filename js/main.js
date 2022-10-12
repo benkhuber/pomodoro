e9547804d25f8f49b267c1c94cfb80f2d98f06cf
Ben Huber <benkhuber@gmail.com>
2022-10-11 18:24:12 -0700

    worldtime function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+let clock = document.querySelector('#clock')
 
+function worldTime () {
+    let hours = new Date().getHours()
+    let minutes = new Date().getMinutes()
+    let seconds = new Date().getSeconds()
+    let session = 'AM'
+
+    if (hours === 0) {
+        hours = 12
+    } else if (hours > 12) {
+        session = 'PM'
+        hours = hours - 12
+    }
+
+    if (hours < 10) {
+        hours = '0' + hours
+    }
+
+    
+clock.innerHTML = `${hours}:${minutes}:${seconds} ${session}`
+}
+
+setInterval(1000)
+
+worldTime()
